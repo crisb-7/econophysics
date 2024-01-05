0dc440b1946566292798208ee1fe3e4cb01241ab
crisb-7 <cristofer.becerra@protonmail.com>
2024-01-04 22:44:21 -0600

    Correct (i think) and simpler simulation design. Removed Tax and Well-Being for now. Add visualization module skeleton (literally)

---
+++ python/plotting.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def line_plot(x, y):
+    pass
+
+def simple_histogram(x, bins):
+    pass
--- python/yakovenko.py
+++ python/yakovenko.py
@@ -1,12 +1,13 @@
+import random
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # TODO: organize constant definitions/make some user-defined
 # TODO: make constants much more descriptive
 # TODO: add readme description of constants
 # TODO: revise implementation details for performance (speed + memory)
 # TODO: develop tests + test code
-# TODO: develop a visualization module 
-#       (this can be used for Wright's model too)
 # TODO: modularize to use delta/uniform initial distribution
 # TODO: add logic to enable/disable tax model
 # TODO: rethink design to implement well-being analysis
@@ -16,67 +17,56 @@ import numpy as np
 N = 1000    # Population (N agents)
 M = 1e5     # Total sum of money in the system
 C = 50      # Number of income classes
+T = 18000    # Total time of the simulation
+
+# Delta amount of money
+nu = 0.05
+dm = (M/N)*nu
 
 # Max amount of money an agent can have
 MMAX = (M/(np.sum(np.arange(C))*(N/C))*C)*2
-
 CLASS = np.linspace(0, MMAX, C)
-T = 1000    # Total time of the simulation
-
-# Delta amount of money
-dm = M*4e-5
-
 bin_centers = 0.5*(CLASS[1:] + CLASS[0:-1])
 
-# Tax
-lambda_ = 0
-tau = 30
-iva = 0
 
-# Well-being
-a = 1/300
-OcaD = [None]*T
-OcbD = [None]*T
-B = [None]*T
-
-delta_distrib = np.ones((N,))*(M/N)
-Sdelta = np.zeros((T,))
+money_distrib = np.ones((N,))*(M/N)
+Entropy = np.zeros((T,))
 
 # TODO: verify correctness - code runs without errors
+    # 04 Jan - Entropy looks correct, simpler design
+verbose = True
+if verbose:
+    print("Number of Agents:", N)
+    print("Time iterations (unit):", T)
+    print("Delta Money:", f"${dm}")
 
 for t in range(T):
-    
-    # histogram visualization used to go here
-    if t % tau == 0:
-        delta_distrib = delta_distrib + iva/N
-        iva = 0
 
-    for j in range(N):
-        l = np.random.randint(N)
-        s = np.sign(2*np.random.rand()-1)
-        
-        if (delta_distrib[j] + dm*s < 0) or delta_distrib[l] - dm*s < 0:
-            continue
-        elif s > 0:
-            delta_distrib[j] = delta_distrib[j] + dm*(1-lambda_)
-            delta_distrib[l] = delta_distrib[l] - dm
-            iva = iva + dm*lambda_
-        else:
-            delta_distrib[j] = delta_distrib[j] - dm
-            delta_distrib[l] = delta_distrib[l] + dm*(1-lambda_)
-            iva = iva + dm*lambda_
-    
-    # Create histogram for money distribution
-    hist, bin_edges = np.histogram(delta_distrib, bins=CLASS)
-    print(id(Sdelta))
-    Sdelta[t] = N*np.log(N) - np.sum(hist*np.log(hist, where=(hist>0)))
+    # Choose two agents
+    agent_i = random.randint(0, N-1)
+    agent_j = random.randint(0, N-1)
+    while agent_i == agent_j:
+        agent_j = random.randint(0, N-1)
 
-    # Well being functions
-    OcaD[t] = sum(a*bin_centers*hist)
-    OcbD[t] = sum(hist*(1-np.exp(-a*bin_centers)))
+    s = random.choice([-1, 1])
+
+    if (money_distrib[agent_i] + s*dm < 0) or (money_distrib[agent_j] - s*dm < 0):
+        continue
+
+    money_distrib[agent_i] = money_distrib[agent_i] + s*dm
+    money_distrib[agent_j] = money_distrib[agent_j] - s*dm
     
-    VEm = 0
-    for u in range(C-1):
-        VEm = VEm + bin_centers[u]*hist[u]/N
-    B[t] = VEm
-    VEm = 0
+    # Histogram for money distribution
+    hist, bin_edges = np.histogram(money_distrib, bins=CLASS)
+    
+    # Entropy function
+    Entropy[t] = N*np.log(N) - np.sum(hist*np.log(hist, where=(hist>0)))
+
+# TODO: develop a visualization module 
+# This is just a rough visualization
+fig, ax = plt.subplots(1, 2, figsize=(10,4))
+ax[0].plot(Entropy)
+
+print(hist)
+sns.histplot(x=hist, bins=bin_edges, ax=ax[1])
+plt.show()
