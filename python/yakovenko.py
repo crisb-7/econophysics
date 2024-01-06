5c7e427e1f374bb443f0e5875334c65d297a85cd
crisb-7 <cristofer.becerra@protonmail.com>
2024-01-06 14:57:59 -0600

    Fix entropy, modularize to funcs, adopt script structure, change dumb plot things

--- python/yakovenko.py
+++ python/yakovenko.py
@@ -1,72 +1,85 @@
 import random
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+from plotting import plot_entropy, plot_money_hist
 
-# TODO: organize constant definitions/make some user-defined
-# TODO: make constants much more descriptive
-# TODO: add readme description of constants
-# TODO: revise implementation details for performance (speed + memory)
-# TODO: develop tests + test code
+# TODO: rethink design - implement well-being & tax 
+# TODO: organize constant definitions/make; script?
 # TODO: modularize to use delta/uniform initial distribution
-# TODO: add logic to enable/disable tax model
-# TODO: rethink design to implement well-being analysis
-# TODO: Translate Entropy Plots
-# TODO: Well-being Boltzmann-Gibbs distribution
+# TODO: make constants much more descriptive
+# TODO: revise implementation details for performance (speed + memory)
+# TODO: add readme description of constants
+# TODO: develop tests + test code
+# TODO: develop a visualization module 
 
-N = 1000    # Population (N agents)
-M = 1e5     # Total sum of money in the system
-C = 50      # Number of income classes
-T = 18000    # Total time of the simulation
+def main():
+    N = 1000    # Population (N agents)
+    M = 1e5     # Total sum of money in the system
+    C = 50      # Number of income classes
+    T = 1000    # Total time of the simulation
 
-# Delta amount of money
-nu = 0.05
-dm = (M/N)*nu
+    # Delta amount of money
+    nu = 0.05
+    dm = (M/N)*nu
 
-# Max amount of money an agent can have
-MMAX = (M/(np.sum(np.arange(C))*(N/C))*C)*2
-CLASS = np.linspace(0, MMAX, C)
-bin_centers = 0.5*(CLASS[1:] + CLASS[0:-1])
+    # Max amount of money an agent can have
+    MMAX = (M/(np.sum(np.arange(C))*(N/C))*C)*2
+    CLASS = np.linspace(0, MMAX, C)
+    bin_centers = 0.5*(CLASS[1:] + CLASS[0:-1])
 
+    money_distrib = np.ones((N,))*(M/N)
+    Entropy = np.zeros((T,))
 
-money_distrib = np.ones((N,))*(M/N)
-Entropy = np.zeros((T,))
+    # TODO: verify correctness - code runs without errors
+        # 06 Jan - Weird entropy drops T > 18000
+    verbose = True
+    if verbose:
+        print("Number of Agents:", N)
+        print("Time iterations (unit):", T)
+        print("Delta Money:", f"${dm}")
 
-# TODO: verify correctness - code runs without errors
-    # 04 Jan - Entropy looks correct, simpler design
-verbose = True
-if verbose:
-    print("Number of Agents:", N)
-    print("Time iterations (unit):", T)
-    print("Delta Money:", f"${dm}")
+    for t in range(T):
 
-for t in range(T):
+        # Choose two agents
+        agent_i, agent_j = choose_agents(N)
 
-    # Choose two agents
-    agent_i = random.randint(0, N-1)
-    agent_j = random.randint(0, N-1)
-    while agent_i == agent_j:
-        agent_j = random.randint(0, N-1)
+        # Perform money exchange between them
+        money_distrib = exchange_money(agent_i, agent_j, money_distrib, dm)
+        
+        # Histogram for money distribution
+        hist, bin_edges = np.histogram(money_distrib, bins=CLASS)
+        
+        # Entropy function
+        entropy = N*np.log(N) - np.sum(hist[hist>0]*np.log(hist[hist>0]))
+        Entropy[t] = entropy
 
+    # note: these pop up one at a time
+    plot_entropy(Entropy, (10,4))
+    plot_money_hist(hist, CLASS, (10,4))
+
+def choose_agents(n_population: int):
+    i = random.randint(0, n_population-1)
+    j = random.randint(0, n_population-1)
+    while i == j:
+        j = random.randint(0, n_population-1)
+    return i, j
+
+def exchange_money(agent_i: int, agent_j: int, distribution: np.array, money: int) -> np.array:
+    """"Perform money exchange between agents.
+
+    Returns
+    -------
+        distribution: money distribution updated with the money exchange
+    """
+    # Determine which agent will win/lose money
     s = random.choice([-1, 1])
 
-    if (money_distrib[agent_i] + s*dm < 0) or (money_distrib[agent_j] - s*dm < 0):
-        continue
-
-    money_distrib[agent_i] = money_distrib[agent_i] + s*dm
-    money_distrib[agent_j] = money_distrib[agent_j] - s*dm
+    if (distribution[agent_i] + s*money < 0) or (distribution[agent_j] - s*money < 0):
+        return distribution
     
-    # Histogram for money distribution
-    hist, bin_edges = np.histogram(money_distrib, bins=CLASS)
-    
-    # Entropy function
-    Entropy[t] = N*np.log(N) - np.sum(hist*np.log(hist, where=(hist>0)))
+    distribution[agent_i] = distribution[agent_i] + s*money
+    distribution[agent_j] = distribution[agent_j] - s*money
 
-# TODO: develop a visualization module 
-# This is just a rough visualization
-fig, ax = plt.subplots(1, 2, figsize=(10,4))
-ax[0].plot(Entropy)
+    return distribution
 
-print(hist)
-sns.histplot(x=hist, bins=bin_edges, ax=ax[1])
-plt.show()
+if __name__ == "__main__":
+    main()
